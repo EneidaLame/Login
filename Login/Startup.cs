cbd1340c92f1ba0d04ff4749999f42c9c88ec5d0
eneidalame <eneida_lame@hotmail.com>
2022-05-23 23:12:09 +0300

    login

---
+++ Login/Startup.cs
@@ -0,0 +1,124 @@
+using Login.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    public class Startup
+    {
+       
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                 .AddDefaultUI()
+                 .AddDefaultTokenProviders()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+
+
+            CreateRoles(serviceProvider).Wait();
+            CreateUsers(serviceProvider).Wait();
+
+
+        }
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            //initializing custom roles
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            var role = new IdentityRole();
+            role.Name = "Admin";
+            await RoleManager.CreateAsync(role);
+
+            role = new IdentityRole();
+            role.Name = "User";
+            await RoleManager.CreateAsync(role);
+        }
+
+        private async Task CreateUsers(IServiceProvider serviceProvider)
+        {
+            //initializing custom roles
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            var firstrole = await RoleManager.FindByIdAsync("User");
+
+            var user = new IdentityUser();
+            user.UserName = "user@hotmail.com";
+            user.Email = "user@hotmail.com";
+            user.EmailConfirmed = true;
+            string userpass = "User123!";
+
+            IdentityResult confUser = await UserManager.CreateAsync(user, userpass);
+
+            if (confUser.Succeeded)
+            {
+                var result = await UserManager.AddToRoleAsync(user, "User");
+            }
+
+            
+        }
+      
+
+    }
+}
